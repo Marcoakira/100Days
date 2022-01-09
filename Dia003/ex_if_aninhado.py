bf96f131cd7a915a6b329360ee3f3706f8095a8b
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-09 20:26:10 -0300

    dia 3 que perrengue

--- Dia003/ex_if_aninhado.py
+++ Dia003/ex_if_aninhado.py
@@ -5,11 +5,21 @@ altura_permitida = 120
 if height >= altura_permitida:
   idade = int(input("Qual sua idade?"))
   if idade < 12:
-    print("o valor da entrada é 5 reais.")
+    entrada = 5
+    print("o valor da entrada  criança é 5 reais.")
   elif idade <=18:
-    print("O valor da entrada é 7 reais.")
+    entrada = 7
+    print("O valor da entrada adolescente é 7 reais.")
   else:
-    print("O valor da entrada é 12 reais.")
+    entrada = 12
+    print("O valor da entrada adulto é 12 reais.")
+
+  fotos = input("voce deseja tirar uma foto por 3 reais? S/N").strip().upper()
+  if fotos == "S":
+    entrada += 3
+    print(f" valor total da entra com foto é de {entrada}")
+
   print("bom passeio")
+
 else:
   print("sinto muito, mas vc não tem altura suficiente")
