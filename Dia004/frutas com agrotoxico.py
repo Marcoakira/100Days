bd704917fc9c9d6abc739aa3e04d7a7785c5f6f2
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-13 21:04:16 -0300

    dia 4 exercicios

---
+++ Dia004/frutas com agrotoxico.py
@@ -0,0 +1,7 @@
+frutas = ["Morangos", "Nectarinas", "Maçãs", "Uvas", "Pêssegos", "Cerejas", "Peras"]
+vegetais = ["Espinafre", "Couve", "Tomate", "Aipo", "Batatas"]
+vegetais2 = ["Espinafre", "Couve", "Tomate", "Aipo", "Batatas"]
+
+dirty_dozen = [frutas, vegetais, vegetais2]
+
+print(dirty_dozen[0][1])
