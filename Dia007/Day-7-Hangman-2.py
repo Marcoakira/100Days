9934c7d795768b72992435f0b8aeb4c44ff73121
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-21 20:56:39 -0300

    Dia 7 acabou

--- Dia007/Day-7-Hangman-2.py
+++ Dia007/Day-7-Hangman-2.py
@@ -1,41 +1,17 @@
-import random
+#Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
+"""TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word."""
+import random
+chosen_word = random.choice(word_list)
 
-palavra_escolhida_pc = random.choice(word_list)
-palavra_escolhida = list(palavra_escolhida_pc)
-palavra_jogador_pc = "_"*len(palavra_escolhida)
-palavra_jogador = list(palavra_jogador_pc)
-print(palavra_escolhida)
-print("#"*78)
-print("SEJA BEM VINDOS AO SUPER MEGA HIPER JOGO DA FORCA, TENTE NAO MORRER HAHAHAHHA")
-print("#"*78)
-# torcar por um len de letras faladas.
-letras_faladas = []
-while palavra_escolhida != palavra_jogador:
-    letra = input(" Escolha uma letra")
-
-    vezes_que_aparecem = 0
-    for l in range(0,len(palavra_escolhida)):
-
-        if palavra_escolhida[l] == letra:
-            palavra_jogador[l] = letra
-            vezes_que_aparecem +=1
-
-    if vezes_que_aparecem == 0:
-        letras_faladas.append(letra)
-
-    print(f"apareceram {vezes_que_aparecem}Vezes a letra {letra}")
-
-    print(palavra_jogador)
-    print(f"voce ja disse errado as letras:{letras_faladas}")
-    print(f"você ainda tem {5-len(letras_faladas)} chances")
-    if len(letras_faladas) >= 5:
-        print(f"sinto muito voce perdeu, a palavra era {palavra_escolhida_pc}")
-        break
-if palavra_escolhida == palavra_jogador:
-
-    print(f"parabens voce ganhou acertou a palavra: {palavra_escolhida_pc}")
-
-
+"""TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase."""
+guess = input("Guess a letter: ").lower()
+"""TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
+# TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word."""
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
