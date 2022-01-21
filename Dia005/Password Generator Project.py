0b253449cc992493179c6c6650f6934640c79865
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-20 22:38:30 -0300

    Fim do sexto Dia

--- Dia005/Password Generator Project.py
+++ Dia005/Password Generator Project.py
@@ -11,6 +11,8 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
 sn_letters = []
 sn_symbolos =[]
 sn_numeros = []
