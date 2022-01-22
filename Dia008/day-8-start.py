1de4518c26f699a28125f223956bd199bc488612
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-22 18:35:15 -0300

    Dia 8 inicio

---
+++ Dia008/day-8-start.py
@@ -0,0 +1,19 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("esse é o primeiro print")
+#   print ("esse é o segundo print")
+#   print("Finalmente o terceiro print")
+#
+#
+# greet()
+
+
+def greet_with_name(name):
+  print(f"hello {name}")
+  print(f"How do you fo {name}")
+
+greet_with_name("Marco")
