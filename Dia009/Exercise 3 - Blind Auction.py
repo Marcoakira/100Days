96ddd3bdb386ff8b7516bb66f878215c82718d31
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-26 19:41:50 -0300

    Dia 9 blind auction - start

---
+++ Dia009/Exercise 3 - Blind Auction.py
@@ -0,0 +1,15 @@
+#from replit import clear
+from art import logo
+print(logo)
+
+#HINT: You can call clear() to clear the output in the console.
+
+
+
+lances = {"nome":"janaina","aposta": 125]}
+
+
+lances["mariana"] =["aposta", 125]
+lances["julia"] = ["aposta", 236]
+print(lances)
+print(lances["nome"]["janaina"])
