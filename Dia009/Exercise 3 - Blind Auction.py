90cb6ddc7ca137d72645ab7123da03664e251a24
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-01-27 21:32:12 -0300

    Dia 9 blind auction - fim

--- Dia009/Exercise 3 - Blind Auction.py
+++ Dia009/Exercise 3 - Blind Auction.py
@@ -6,10 +6,27 @@ print(logo)
 
 
 
-lances = {"nome":"janaina","aposta": 125]}
+lances = []
+proximo = "s"
 
 
-lances["mariana"] =["aposta", 125]
-lances["julia"] = ["aposta", 236]
+while proximo == "s":
+    nome = str(input("Qual o seu nome? \n"))
+    aposta = int(input("Qual o valor da sua aposta? \n"))
+    proximo = str(input("Mais alguem vai apostar? S ou N")).lower()
+    lance = {"nome": nome, "aposta": aposta}
+    lances.append(lance)
+
+#lances = [{'nome': 'batata', 'aposta': 152}, {'nome': 'joaquim', 'aposta': 23}, {'nome': 'rafaelo', 'aposta': 26}, {'nome': 'ranito', 'aposta': 30}]
+maior_aposta = {'nome': 'null', 'aposta': 0}
+
+for pos, cont in enumerate(lances):
+    # print(pos)
+    # print(cont)
+    if lances[pos]["aposta"] > maior_aposta["aposta"]:
+        maior_aposta = lances[pos]
+
+print(f"a maior aposta foi de  {maior_aposta['nome']} no valor de {maior_aposta['aposta'] }\n")
+
+
 print(lances)
-print(lances["nome"]["janaina"])
