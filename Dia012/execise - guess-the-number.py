c57334c3016e35ade9ed9d31aedc3923b516e95b
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-02-04 18:40:44 -0300

    Dia 12 - incio

---
+++ Dia012/execise - guess-the-number.py
@@ -0,0 +1,9 @@
+#Number Objetivos do jogo de adivinhação:
+
+# Inclua um logotipo de arte ASCII.
+# Permitir que o jogador envie um palpite para um número entre 1 e 100.
+# Verifica o palpite do usuário em relação à resposta real. Imprima "Muito alto". ou "Muito baixo". dependendo da resposta do usuário.
+# Se eles acertaram a resposta, mostre a resposta real ao jogador.
+# Acompanhe o número de voltas restantes.
+# Se eles ficarem sem turnos, forneça feedback ao jogador.
+# Inclua dois níveis de dificuldade diferentes (por exemplo, 10 palpites no modo fácil, apenas 5 palpites no modo difícil).
