b38f82d11cc42310392f3cb802c75f6daf590e9d
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-02-09 23:10:21 -0300

    Dia 16 - inicio

---
+++ Dia016/day-16-start/main.py
@@ -0,0 +1,49 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column('Pokemon Name',['pikachu', 'Squirtle', 'Charmander'])
+
+table.add_column('Type', ['Electric', 'Water', 'Fire'])
+table.align = "l"
+
+
+
+
+
+
+
+
+print(table)
+
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.color("#285078")
+#
+# timmy.shape("turtle")
+#
+# timmy.forward(250)
+# timmy.forward(-75)
+# timmy.circle(42)
+# timmy.forward(-95)
+#
+# timmy.circle(42)
+# timmy.forward(-95)
+# timmy.circle(42)
+# timmy.forward(-95)
+# timmy.circle(42)
+# timmy.right(36)
+# timmy.forward(42)
+
+
+# my_screen = Screen()
+#
+# print(my_screen.canvheight)
+#
+# my_screen.exitonclick()
+#
+
+
---
+++ projeto_dia016_oop_coffee_machine/main.py
@@ -0,0 +1,13 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+my_menu = Menu
+my_menu_item = MenuItem
+my_coffee_maker = CoffeeMaker
+my_money_machine = MoneyMachine
+
+my_money_machine.report()
+
+
+print()
