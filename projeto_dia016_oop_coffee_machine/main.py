7c568c82b04c03434cf664bb36ad1b9f61e9ce86
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-02-10 23:14:12 -0300

    Dia 16 - fim

--- projeto_dia016_oop_coffee_machine/main.py
+++ projeto_dia016_oop_coffee_machine/main.py
@@ -1,13 +1,23 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_menu = Menu
-my_menu_item = MenuItem
-my_coffee_maker = CoffeeMaker
-my_money_machine = MoneyMachine
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-my_money_machine.report()
+is_on = True
 
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
 
-print()
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
