616ecc28767555e4237bf91bb412a6c286056f91
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-02-21 18:18:47 -0300

    Dia 17 - meio

--- Dia017/Quizz/question_model.py
+++ Dia017/Quizz/question_model.py
@@ -1,11 +1,20 @@
 import data
+
 class Question():
     def __init__(self, numero):
         self.text = data.question_data[numero]["text"]
         self.answer = data.question_data[numero]["answer"]
 
+pontos = 0
 
 questao_0 = Question(0)
 
+def user_question(boleano):
+
+    if boleano and questao_0.answer :
+        pontos += 1
+
 print(questao_0.text)
-print(questao_0.answer)
+
+# print(questao_0.text)
+# print(questao_0.answer)
--- Dia017/teste de POO.py
+++ Dia017/teste de POO.py
@@ -1,27 +1,75 @@
-class User:
-    def __init__(self, user_id, username):
-        self.id = len(user_id)
+#Class
+class User:  # name of class, have initial letter of world in upper. ( PascalCase)
+    # constructor method
+    def __init__(self, user_id, username):  # essa função que é chamada toda vez que for criado um objeto
+        self.id = user_id
         self.username = username
         self.followers = 0
         self.following = 0
 
+
     def follow(self, user):
         user.followers += 1
         self.following += 1
+        user.username = "outro cara tem esse nome"
+        self.username = " eu tenho esse nome"
 
-user_1 = User("001", "nivia")
-user_2 = User("002", "marco")
+# objects
 
-user_1.follow(user_2)
+user_0 = User('0022', 'incrivel')
+user_1 = User('006', 'alguem')
 
+user_0.follow(user_1)
 
+print(user_0.followers)
+print(user_0.following)
 print(user_1.followers)
 print(user_1.following)
 
+print(user_0.username)
+print(user_1.username)
 
-print(user_2.followers)
-print(user_2.following)
 
+
+
+
+
+# user_1 = User()
+# user_1.id = '001'
+# user_1.cocobongo = 'clube'
+# user_1.sexo = 'robo'
+
+# user_2 = User()
+#
+# print(user_1.cocobongo)
+#
+# print(user_2.cocobongo)
+
+
+# class User:
+#     def __init__(self, user_id, username):
+#         self.id = len(user_id)
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+# user_1 = User("001", "nivia")
+# user_2 = User("002", "marco")
+#
+# user_1.follow(user_2)
+#
+#
+# print(user_1.followers)
+# print(user_1.following)
+#
+#
+# print(user_2.followers)
+# print(user_2.following)
+#
 
 #
 # user_1 = User()
@@ -36,3 +84,34 @@ print(user_2.following)
 # user_2.username = "Marco"
 #
 # print(user_2.username)
+
+
+# class Car:
+#     def __init__(self):
+#         self.seats = 5
+#
+#     def entre_race_mode(self):
+#         self.seats = 2
+#
+#
+#
+#
+# raposinha = Car()
+#
+# print(raposinha.entre_race_mode())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
