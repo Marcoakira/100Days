f6a2d3758e8c15fa7689613914a8d5b004036d86
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-03-11 21:36:16 -0300

    Dia 24 inicio

---
+++ Dia024/project_24/main.py
@@ -0,0 +1,16 @@
+
+
+# with open('my_file.txt') as f:
+#     contents = f.read()
+#     print(contents)
+
+
+with open('new_file.txt', mode='a') as f:
+    f.write('\n')
+    f.write('Esse é um novo arquivo')
+    f.write('\n')
+    f.write('My name is marco Aurelio')
+    # f.write('\n')
+    # f.write("new text")
+    # conteudo = f.read()
+    # print(conteudo)
