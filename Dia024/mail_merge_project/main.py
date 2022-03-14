e8af38d5e582191a45527884b2e7891287265da1
Dr Marco Aurelio Menezes <tetissu2@gmail.com>
2022-03-14 14:52:33 -0300

    Dia 24 fim

--- Dia024/mail_merge_project/main.py
+++ Dia024/mail_merge_project/main.py
@@ -1,5 +1,4 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
+
 
 list_names = []
 with open('./Input/Names/invited_names.txt') as name_file:
@@ -20,35 +19,3 @@ with open('./Input/Letters/starting_letter.txt') as letter_file:
 
 
 
-
-
-
-# with open('Input/Letters/starting_letter.txt') as starting_letter:
-#     letter = starting_letter.read()
-# #     print(letter)
-#
-# letra = open('Input/Letters/starting_letter.txt')
-# bomdia = letra.read()
-#
-#
-#
-# name = open('Input/Names/invited_names.txt','r')
-# nname = name.readlines()
-# name = nname.replace('\n','')
-# print(name)
-#
-# testando = open('Output/testando.txt', 'w')
-# testando.write(f'{bomdia}')
-#
-
-
-
-# with open('Output/Letters/invited_letters.txt', 'w') as invited_letters:
-#     for name in names:
-#         invited_letters.write(letter.replace('{name}', name))
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
